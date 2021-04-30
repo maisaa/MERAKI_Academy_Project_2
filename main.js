d025fe00859c5f7f6da7c50a17ad16816d1d7617
maisaa <maisaa.alkhedr@gmail.com>
2021-04-30 15:37:23 +0200

    render Products by onclick

--- main.js
+++ main.js
@@ -2,28 +2,35 @@ const body = $('body')
 let products = $('#products')
 let categories = $('#main-category')
 
-let mainCategory = [{src:"./images/c0.jpg", title:"Coffee",discretion:"Coffee"},
-                    {src:"./images/n2.jpg", title:"Nuts",discretion:"Nuts"},
-                    {src:"./images/cc.jpg", title:"Chocolate",discretion:"Chocolate"}]
+let mainCategory = [{src:"./images/c0.jpg", title:"Coffee",discretion:" delicious roasted and ground coffee, specially designed for our dear customers "},
+                    {src:"./images/n2.jpg", title:"Nuts",discretion:"high quality nuts, tasty every time from Aladdin Roastery to all parts of Germany"},
+                    {src:"./images/cc.jpg", title:"Chocolate",discretion:"wide collection of chocolate made from the finest cocoa beans and the highest European health and quality standards"}]
 
 // let items = ["./images/13.jpeg","./images/14.jpeg","./images/15.jpeg"]
-let items =    [{src:"./images/13.jpeg", title:"Pistachio",discretion:"discretion"},
-                {src:"./images/t1.jpeg", title:"Rocher",discretion:"Rocher & pistachio"},
-                {src:"./images/15.jpeg", title:"Man & Salwa",discretion:"Man & Salwa"},
-                {src:"./images/14.jpeg", title:"Rose",discretion:"discretion"},
-                {src:"./images/t2.jpeg", title:"Wafer",discretion:"Wafer & pistachio"},
-                {src:"./images/11.jpeg", title:"Baby set",discretion:"Baby set"}]
+let Chocolate =    [{src:"./images/13.jpeg", title:"Pistachio",discretion:"discretion"},
+                        {src:"./images/t1.jpeg", title:"Rocher",discretion:"Rocher & pistachio"},
+                        {src:"./images/15.jpeg", title:"Man & Salwa",discretion:"Man & Salwa"},
+                        {src:"./images/14.jpeg", title:"Rose",discretion:"discretion"},
+                        {src:"./images/t2.jpeg", title:"Wafer",discretion:"Wafer & pistachio"},
+                        {src:"./images/11.jpeg", title:"Baby set",discretion:"Baby set"}]
 
+let Nuts = [{src:"./images/n2.jpg", title:"high quality nuts",discretion:"discretion"}]  
 
+let Coffee = [{src:"./images/c1.jpg", title:"Arabic coffee",discretion:"discretion"}] 
+
+// render main Category part1
 let renderMainCategory = () =>{
     mainCategory.forEach((ele,i) => {
         console.log("render list of Categories");
         const card = $ (`<div id=${i} class="card">
                             <div class="image">
-                                <img src="${ele.src}" alt="${ele.title}">
+                                <img src="${ele.src}" alt="${ele.title}" onClick="renderItems(${ele.title})" > 
                             </div>
                             <div class="title">
-                                <h4>${ele.title}</h4>
+                                <h3>${ele.title}</h3>
+                            </div>
+                            <div class="deception">
+                                <p>${ele.discretion}</p>
                             </div>
                         </div>`);
         card.appendTo(categories);    
@@ -32,8 +39,10 @@ let renderMainCategory = () =>{
 }
 renderMainCategory()
 
-let renderItems =()=>{
-    items.forEach( (ele,i) => {
+// render list of products part2
+let renderItems =(category)=>{
+    products.empty()
+    category.forEach( (ele,i) => {
         console.log("render list of items");
         const card =$( `<div id=${i} class="item-card">
                             <div class="image">
@@ -49,7 +58,10 @@ let renderItems =()=>{
 }
 renderItems()
 
-// const nuts = $("#category-1-1")
+
+// hide and show
+
+// const nuts = $("#category-1-")
 
 // $(document).ready(function(){
 //     $("#category-1-1").click(function(){
