1cf36c45f33d6e2db1523ac6348ccdde0a87ebdf
maisaa <maisaa.alkhedr@gmail.com>
2021-04-30 12:05:43 +0200

    render Categories list

--- main.js
+++ main.js
@@ -1,18 +1,42 @@
 const body = $('body')
 let products = $('#products')
+let categories = $('#main-category')
+
+let mainCategory = [{src:"./images/c0.jpg", title:"Coffee",discretion:"Coffee"},
+                    {src:"./images/n2.jpg", title:"Nuts",discretion:"Nuts"},
+                    {src:"./images/cc.jpg", title:"Chocolate",discretion:"Chocolate"}]
 
 // let items = ["./images/13.jpeg","./images/14.jpeg","./images/15.jpeg"]
-let items = [{src:"./images/13.jpeg", title:"title",discretion:"discretion"},
-             {src:"./images/t1.jpeg", title:"title",discretion:"discretion"},
-             {src:"./images/15.jpeg", title:"title",discretion:"discretion"},
-             {src:"./images/14.jpeg", title:"title",discretion:"discretion"},
-             {src:"./images/t2.jpeg", title:"title",discretion:"discretion"},
-             {src:"./images/11.jpeg", title:"title",discretion:"discretion"}]
+let items =    [{src:"./images/13.jpeg", title:"title",discretion:"discretion"},
+                {src:"./images/t1.jpeg", title:"title",discretion:"discretion"},
+                {src:"./images/15.jpeg", title:"title",discretion:"discretion"},
+                {src:"./images/14.jpeg", title:"title",discretion:"discretion"},
+                {src:"./images/t2.jpeg", title:"title",discretion:"discretion"},
+                {src:"./images/11.jpeg", title:"title",discretion:"discretion"}]
+
+
+let renderMainCategory = () =>{
+    mainCategory.forEach((ele,i) => {
+        console.log("render list of Categories");
+        const card = $ (`<div id=${i} class="card">
+                            <div class="image">
+                                <img src="${ele.src}">
+                            </div>
+                            <div class="title">
+                                <h4>${ele.title}</h4>
+                            </div>
+                        </div>`);
+        card.appendTo(categories);    
+    });
+
+}
+
+renderMainCategory()
 
 let renderItems =()=>{
     items.forEach( (ele,i) => {
-        console.log("render list");
-        const card =$( `<div id=${ele.title} class="card" style="max-width: 20% ; max-height: 40%;">
+        console.log("render list of items");
+        const card =$( `<div id=${i} class="card" style="max-width: 20% ; max-height: 40%;">
                         <img class="card-img-top" src="${ele.src}" alt="Card image cap">
                         <div class="card-body">
                             <h5 class="card-title">${ele.title}</h5>
